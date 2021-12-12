5a8c59eb008c18507a9d8c3014937082babefca9
silical <SilicalNZ@gmail.com>
2021-12-12 14:47:24 +0100

    Remove test input

--- AoC/2021/day11.py
+++ AoC/2021/day11.py
@@ -6,33 +6,6 @@ import pprint
 
 
 data = ""
-data = """start-YY
-av-rz
-rz-VH
-fh-av
-end-fh
-sk-gp
-ae-av
-YY-gp
-end-VH
-CF-qz
-qz-end
-qz-VG
-start-gp
-VG-sk
-rz-YY
-VH-sk
-rz-gp
-VH-av
-VH-fh
-sk-rz
-YY-sk
-av-gp
-rz-qz
-VG-start
-sk-fh
-VG-av"""
-
 
 START = 'start'
 END = 'end'
