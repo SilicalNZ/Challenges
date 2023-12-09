24618a1acc1186699786aa2dbd823e4dbc2fb4fa
SilicalNZ <SIlicalNZ@gmail.com>
2023-12-09 10:16:34 +0100

    Fix incorrect card list

--- AoC/2023/day07.py
+++ AoC/2023/day07.py
@@ -2,7 +2,7 @@ data = """"""
 
 
 cards = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
-p2_cards = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
+p2_cards = "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".split(", ")
 
 p2 = False
 
